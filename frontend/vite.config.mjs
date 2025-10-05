329b82474053250652c9f61c5aef025c4f1a8155
Juan <josemariacosta_8@hotmail.com>
2025-10-04 19:51:58 -0500

    fix: changed output directory to dist instead of ../static/dist

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -8,7 +8,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
-    outDir: path.resolve(__dirname, '../static/dist'),
+    outDir: 'dist',
     emptyOutDir: true,
   },
   base: '/',
